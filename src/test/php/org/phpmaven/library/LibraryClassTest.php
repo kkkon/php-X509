666792725dcf9c7cf860a50e198a6c16c08997b4
kkkon <kiyofumi.kondoh+github@gmail.com>
2013-06-06 19:41:28 +0900

    initial

---
+++ src/test/php/org/phpmaven/library/LibraryClassTest.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'org/phpmaven/library/LibraryClass.php';
+
+/**
+ * Sekeltron
+ */
+class LibraryClassTest extends PHPUnit_Framework_TestCase {
+
+	public function testMe()
+	{
+		//TODO something test stuff.
+	}
+	
+
+}//CLASS
+
